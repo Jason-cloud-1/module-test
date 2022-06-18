0f8ebada8da7b7ff5efc02054a5ed5dbe7e49ea9
EC2 Default User <3137234765@qq.com>
2022-06-18 17:17:32 +0000

    9

--- module-example/ec2/ami-ids.tf
+++ module-example/ec2/ami-ids.tf
@@ -5,16 +5,15 @@ locals {
 }
 
 data "aws_ami" "amazonlinux2" {
-  owners = ["amazon"]
   most_recent = true
-
+  
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
-
   filter {
-    name   = "state"
-    values = ["available"]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
+  owners = ["amazon"] # Canonical
 }
